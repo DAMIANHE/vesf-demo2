6179da9da61358408f3cb64ac7d09101bc9cd60b
DAMIANHE <66206811+DAMIANHE@users.noreply.github.com>
2025-12-18 07:37:32 -0300

    Create exampleData.js

---
+++ data/exampleData.js
@@ -0,0 +1,34 @@
+const exampleData = [
+  {
+    variable: "Inflación",
+    supuesto: 60,
+    realidad: 72,
+    peso: 0.18,
+    impactoVANPercent: -4.6,
+    impactoVANDollars: -2300000,
+    riesgo: "Crítico",
+    recomendacion: "Ajustar precios y estructura de costos"
+  },
+  {
+    variable: "Morosidad",
+    supuesto: 55,
+    realidad: 68,
+    peso: 0.22,
+    impactoVANPercent: -5.1,
+    impactoVANDollars: -2550000,
+    riesgo: "Crítico",
+    recomendacion: "Revisar políticas de crédito"
+  },
+  {
+    variable: "Fondeo",
+    supuesto: 50,
+    realidad: 55,
+    peso: 0.12,
+    impactoVANPercent: -1.4,
+    impactoVANDollars: -700000,
+    riesgo: "Medio",
+    recomendacion: "Diversificar fuentes de fondeo"
+  }
+];
+
+export default exampleData;
